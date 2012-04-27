20f242ab13beba70a03003c0bc34fb88963d4efe
shrikant <shrikant.l@homework.do>
2012-04-27 15:33:40 +0530

    all

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/octapress/"
+http_images_path = "/octapress/images"
+http_fonts_path = "/octapress/fonts"
+css_dir = "public/octapress/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
